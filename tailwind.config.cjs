ad5dc5d71e84968cde761b565b8326e2f84c9715
duongducnguyen <duongnguyen.geek@gmail.com>
2026-05-31 14:34:28 +0700

    feat(appearance): add --canvas token (#121212) and bg-canvas color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
+        canvas: 'hsl(var(--canvas))',
         foreground: 'hsl(var(--foreground))',
         primary: {
           DEFAULT: 'hsl(var(--primary))',
